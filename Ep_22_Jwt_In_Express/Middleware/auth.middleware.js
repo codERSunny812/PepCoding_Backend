26fe9633c1a4262018c5d16faca86abc8aefdfd7
codersunny812 <sengersunny448@gmail.com>
2025-01-15 14:26:18 +0530

    jwt in express

---
+++ Ep_22_Jwt_In_Express/Middleware/auth.middleware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+
+const SECRET_KEY="this is the secret  key"
+
+const authMiddleWare =(req,res,next,err)=>{
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).json({ message: "Token is required" });
+    }
+
+    try {
+        const decoded = jwt.verify(token, SECRET_KEY);
+        req.user = decoded; // Attach user data to the request object
+        next();
+    } catch (error) {
+        res.status(401).json({ message: "Invalid or expired token" });
+    }
+}
+
+
+module.exports=authMiddleWare
---
+++ Ep_22_Jwt_In_Express/Model/user.modal.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    age: {
+        type: Number,
+        default: 18,
+    },
+    password:{
+        type:String,
+        required:true
+    }
+});
+
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
